0cf6cebf94a768e54fa6d87ef28f2883c2d82319
maks <maxim_86nva@mail.ru>
2023-08-12 10:54:42 +0300

    init project

---
+++ src/main/java/com/example/hwspringind10/HwSpringInd10Application.java
@@ -0,0 +1,13 @@
+package com.example.hwspringind10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSpringInd10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwSpringInd10Application.class, args);
+    }
+
+}
