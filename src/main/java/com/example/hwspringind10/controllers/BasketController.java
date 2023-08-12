6bd3b0e09aa86551293b300a5dcfa7bda7693968
maks <maxim_86nva@mail.ru>
2023-08-12 15:13:06 +0300

    пример отлова ошибки в контроллере

--- src/main/java/com/example/hwspringind10/controllers/BasketController.java
+++ src/main/java/com/example/hwspringind10/controllers/BasketController.java
@@ -1,10 +1,10 @@
 package com.example.hwspringind10.controllers;
 
 import com.example.hwspringind10.servicies.BasketService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.List;
 import java.util.Map;
@@ -12,6 +12,11 @@ import java.util.Map;
 @RestController
 @RequestMapping("/order")
 public class BasketController {
+
+    @ExceptionHandler({MethodArgumentTypeMismatchException.class, NumberFormatException.class})
+    public String handleException(MethodArgumentTypeMismatchException e) {
+        return "Не тот формат ввода";
+    }
 
     private final BasketService basketService;
 
